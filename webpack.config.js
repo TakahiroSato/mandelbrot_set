c15b09b640b2457347ab5d8456804334327a3f3f
TakahiroSato <takahiro.sato@aijus.com>
2019-10-18 15:20:06 +0900

    スラッシュ抜け

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    publicPath: "dist",
+    publicPath: "dist/",
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js"
   },
