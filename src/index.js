d4fa5586a342d39f9bd7ffbda469679c4ac2d652
TakahiroSato <takahiro.sato@aijus.com>
2019-10-18 18:55:07 +0900

    速度アップ

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const mandelbrot_set_wasm = import("mandelbrot_set_wasm");
+const mod = import("mandelbrot_set_wasm");
 
-mandelbrot_set_wasm.then(mandelbrot_set_wasm => {
+mod.then((mod) => {
   const canvas = document.getElementById("canvas");
   const ctx = canvas.getContext("2d");
 
@@ -37,22 +37,12 @@ mandelbrot_set_wasm.then(mandelbrot_set_wasm => {
 
   const draw = () => {
     ctx.clearRect(0, 0, width, height);
-    const imgData = ctx.createImageData(width, height);
-    for (let i = 0; i < height; i++) {
-      for (let j = 0; j < width; j++) {
-        const tx = (j - x) / mag;
-        const ty = (i - y) / mag;
-        const color = mandelbrot_set_wasm.mandelbrot(tx, ty, calcMax);
-        if (color < 255) {
-          imgData.data[j * 4 + i * imgData.width * 4] = color;
-          imgData.data[1 + j * 4 + i * imgData.width * 4] = color;
-          imgData.data[2 + j * 4 + i * imgData.width * 4] = color;
-          imgData.data[3 + j * 4 + i * imgData.width * 4] = 255;
-        }
-      }
-
-      ctx.putImageData(imgData, 0, 0);
-    }
+    const imgData = new ImageData(
+      Uint8ClampedArray.from(mod.img_gen(width, height, calcMax, x, y, mag)),
+      width,
+      height
+    );
+    ctx.putImageData(imgData, 0, 0);
   };
 
   document.getElementById("slide1").addEventListener("input", e => {
